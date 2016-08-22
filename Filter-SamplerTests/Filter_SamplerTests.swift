46b8f13d9e491e4bb5cb4107d8597318f90fbb30
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-08-23 03:22:05 +0900

    initial commit

---
+++ Filter-SamplerTests/Filter_SamplerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Filter_SamplerTests.swift
+//  Filter-SamplerTests
+//
+//  Created by ShinokiRyosei on 2016/08/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import XCTest
+@testable import Filter_Sampler
+
+class Filter_SamplerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
