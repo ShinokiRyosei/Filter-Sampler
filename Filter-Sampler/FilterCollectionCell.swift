0905099fae5fa3d6533a4755e9d1a4bb058c5a7c
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-08-23 11:58:52 +0900

    [Create] Filter Collection Cell

---
+++ Filter-Sampler/FilterCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilterCollectionCell.swift
+//  Filter-Sampler
+//
+//  Created by ShinokiRyosei on 2016/08/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
